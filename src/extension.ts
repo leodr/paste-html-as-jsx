d424c5e7434a88470e6af3f1a7567eff2e27b92a
Leo Driesch <leo.driesch@gmail.com>
2020-11-20 00:27:15 +0100

    :art: Set up prettier

--- src/extension.ts
+++ src/extension.ts
@@ -3,26 +3,26 @@ import * as HtmlToJsx from "htmltojsx";
 import * as vscode from "vscode";
 
 export function activate() {
-    const { clipboard } = vscode.env;
+  const { clipboard } = vscode.env;
 
-    vscode.window.onDidChangeWindowState(async (state) => {
-        if (state.focused) {
-            const clipboardText = await clipboard.readText();
+  vscode.window.onDidChangeWindowState(async (state) => {
+    if (state.focused) {
+      const clipboardText = await clipboard.readText();
 
-            const trimmedClipboardText = clipboardText.trim();
+      const trimmedClipboardText = clipboardText.trim();
 
-            const clipboardIsHtml =
-                trimmedClipboardText.startsWith("<") &&
-                trimmedClipboardText.endsWith(">");
+      const clipboardIsHtml =
+        trimmedClipboardText.startsWith("<") &&
+        trimmedClipboardText.endsWith(">");
 
-            if (clipboardIsHtml) {
-                const converter = new HtmlToJsx({
-                    createClass: false,
-                });
-                let result = converter.convert(trimmedClipboardText);
+      if (clipboardIsHtml) {
+        const converter = new HtmlToJsx({
+          createClass: false,
+        });
+        let result = converter.convert(trimmedClipboardText);
 
-                await clipboard.writeText(result);
-            }
-        }
-    });
+        await clipboard.writeText(result);
+      }
+    }
+  });
 }
