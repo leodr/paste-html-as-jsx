4432e18b6e1110ba9742ad760823d763bf390640
Leo Driesch <leo.driesch@gmail.com>
2020-11-22 17:58:24 +0100

    :white_check_mark: Set up testing

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+/**
+ * This Babel config is only used by Jest.
+ */
+module.exports = {
+    presets: [
+        ["@babel/preset-env", { targets: { node: "current" } }],
+        "@babel/preset-typescript",
+    ],
+};
