5e84a876259674847cc7dc17e257109ffecb7019
Leo Driesch <leo.driesch@gmail.com>
2020-12-25 17:47:50 +0100

    :bug: Fix typescript errors

--- src/test/test-utils.ts
+++ src/test/test-utils.ts
@@ -1,4 +1,9 @@
 /**
  * Just to provide Prettier formatting and syntax
  */
-export const html = ([str]: TemplateStringsArray) => str;
+export const html = ([str]: TemplateStringsArray) => {
+    if (!str) {
+        throw Error("No string specified");
+    }
+    return str;
+};
