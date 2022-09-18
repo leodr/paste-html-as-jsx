dbc4ea2fefe9b5918fe468fd04d35de17ab70be6
Leo Driesch <dev@leodriesch.com>
2022-09-18 16:34:25 +0200

    :fire: Remove tests as they now live in html transformer library

--- src/test/test-utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Just to provide Prettier formatting and syntax
- */
-export const html = ([str]: TemplateStringsArray) => {
-    if (!str) {
-        throw Error("No string specified");
-    }
-    return str;
-};
